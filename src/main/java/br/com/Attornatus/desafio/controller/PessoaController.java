eb308a1d5eb16171841fab2cc29bcda88b38cc2d
DavidPaulinus <davipm@hotmail.com>
2023-02-07 13:38:15 -0300

    feito consultar pessoa

--- src/main/java/br/com/Attornatus/desafio/controller/PessoaController.java
+++ src/main/java/br/com/Attornatus/desafio/controller/PessoaController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +48,12 @@ public class PessoaController {
 		System.out.println("***Listando***");
 		
 		return ResponseEntity.ok(pRep.findAll(page).map(PessoaListarDTO::new));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PessoaDetalhamentoDTO> consultarPessoa(@PathVariable Long id) {
+		System.out.println("***Detalhando***");
+		
+		return ResponseEntity.ok(new PessoaDetalhamentoDTO(pRep.getReferenceById(id)));
 	}
 }
